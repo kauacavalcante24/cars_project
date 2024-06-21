0f1fa5176929274d641a42424561ce5ee38f7f08
Kauã Silva <156628493+kauacavalcante24@users.noreply.github.com>
2024-06-21 16:15:58 -0300

    Alterações nos templates

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projeto Carros
+Projeto de gestão de carros desenvolvido com o framework Django Python. O sistema oferece funcionalidades para controle de veículos. Além de poder integrar-se a API do GEMINI para gerar descrições, em caso de vazias.
+
+## Imagem do projeto 
+
+### Listagem dos Carros Cadastrados
+![image](./static/img/example-image/github_carslist.png)
--- app/urls.py
+++ app/urls.py
@@ -9,6 +9,8 @@ from account.views import create_account, login_view, logout_view
 urlpatterns = [
     #     Rota           #View
     path('admin/', admin.site.urls),
+    path('', CarListView.as_view()),
+
     path('account/', create_account, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
@@ -17,5 +19,6 @@ urlpatterns = [
     path('new_car/', NewCarCreateView.as_view(), name='new_cars_list'),
     path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'), #pk = primary key
     path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
-    path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete')
+    path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #configuração para imagens
